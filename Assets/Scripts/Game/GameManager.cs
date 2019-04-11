ee295b7dfc4d1643ea210a65db658426ddc8a443
Novacane01 <mbionwuc@ufl.edu>
2019-04-11 00:42:26 -0400

    Pause menu

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -9,17 +9,24 @@ public class GameManager : MonoBehaviour
     public static int numPlayers;
     public static Player[] players;
     public static Dice dice;
+    public UIManager UI;
     // Start is called before the first frame update
     void Start()
     {
-        
+        UI.GetComponentInChildren<Canvas>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("escape")) {
-            Application.Quit();
-        } 
+    }
+
+    public void pause() {
+        if (UI.GetComponentInChildren<Canvas>().enabled) {
+            UI.GetComponentInChildren<Canvas>().enabled = false;
+        }
+        else {
+            UI.GetComponentInChildren<Canvas>().enabled = true;
+        }
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-using TMPro;
 public class UIManager : MonoBehaviour
 {
     protected PlayerController player;
+    public GameManager GM;
+    public MusicManager MM;
+
+    private Slider volume;
 
     void Start(){
         player = PlayerManager.instance.player.GetComponent<PlayerController>();
+        volume = GameObject.Find("Volume").GetComponent<Slider>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("escape")) {
+            GM.pause();
+        }
+    }
+
+    public void volumeUpdate(float vol) {
+        MM.SetVolume(vol);
+    }
+
+    public void toggleMusic(bool toggle) {
+        volume.interactable = toggle;
+        if (toggle == true) {
+            MM.SetVolume(volume.value);
+        } 
+        else {
+            MM.SetVolume(0f);
+        }
     }
     public void Enable(string name){
         foreach(Transform t in transform){
